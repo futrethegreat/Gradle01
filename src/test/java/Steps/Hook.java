0b283dca2cd998f5e02c92cc005953d1642f0ba4
futrethegreat <dsauce@gmail.com>
2018-03-15 13:01:04 +0100

    Generalizacion de variables para browser en Hook.java

--- src/test/java/Steps/Hook.java
+++ src/test/java/Steps/Hook.java
@@ -6,10 +6,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
+import Base.BaseUtil;
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
-import Base.BaseUtil;
 
 /**
  * @author DavidSauce
@@ -17,75 +17,105 @@ import Base.BaseUtil;
  */
 public class Hook extends BaseUtil {
 
-  private BaseUtil base;
-  // private static final String SearchTermsFile = "C:\\Libs\\SearchTerms.xls";
-  private static final String ChromeDriverFile = "C:\\Libs\\chromedriver.exe";
-  private static final String ChromeDriverType = "webdriver.chrome.driver";
-  private static final String FirefoxDriverFile = System.getProperty("user.dir") + "/src/resources/geckodriver";
-//  private static final String FirefoxDriverFile = "C:\\Libs\\geckodriver.exe";
-  private static final String FirefoxDriverType = "webdriver.gecko.driver";
-  private static final String BROWSER = "FF"; // Options can be CH FF IE SF OP
+	private BaseUtil base;
+	// private static final String SearchTermsFile = "C:\\Libs\\SearchTerms.xls";
+	private static final String ChromeDriverFileLinux = System.getProperty("user.dir") + "/src/resources/chromedriver";
+	private static final String ChromeDriverFileWindows = "C:\\Libs\\chromedriver.exe";
+	private static final String ChromeDriverType = "webdriver.chrome.driver";
+	private static final String FirefoxDriverFileLinux = System.getProperty("user.dir") + "/src/resources/geckodriver";
+	private static final String FirefoxDriverFileWindows = "C:\\Libs\\geckodriver.exe";
+	private static final String FirefoxDriverType = "webdriver.gecko.driver";
+	private static final String BROWSER = "FF"; // Options can be CH FF IE SF OP
+	private static final String OperatingSystem = System.getProperty("os.name").toLowerCase();
+	private static String DriverFile;
+	private static String DriverType;
 
-  public Hook(BaseUtil base) {
-    this.base = base;
-  }
+	public Hook(BaseUtil base) {
+		this.base = base;
+	}
 
-  @Before
-  public void InitializeTest() {
-    System.out.println("Opening browser: " + BROWSER);
-    switch (BROWSER) {
-    case "CH":
-      base.driver = openChrome();
-      break;
-    case "FF":
-      base.driver = openFirefox();
-      System.out.println("Opened FF");
-      break;
-    default:
-      base.driver = openFirefox();
-    }
-  }
+	@Before
+	public void InitializeTest() {
+		if (OperatingSystem.contains("win") && (BROWSER == "FF")) {
+			DriverFile = FirefoxDriverFileWindows;
+			DriverType = FirefoxDriverType;
+		} else if (OperatingSystem.contains("win") && (BROWSER == "CH")) {
+			DriverFile = ChromeDriverFileWindows;
+			DriverType = ChromeDriverType;
+		} else if (OperatingSystem.contains("nux") && (BROWSER == "FF")) {
+			DriverFile = FirefoxDriverFileLinux;
+			DriverType = FirefoxDriverType;
+		} else if (OperatingSystem.contains("nux") && (BROWSER == "CH")) {
+			DriverFile = ChromeDriverFileLinux;
+			DriverType = ChromeDriverType;
+		}
+		base.driver = openBrowser();
 
-  @After
-  public void TearDownTest(Scenario scenario) {
-    if (scenario.isFailed()) {
-      // Scenario fallido
-    }
-    // Get Browser name and version.
-    Capabilities caps = ((RemoteWebDriver) base.driver).getCapabilities();
-    String browserName = caps.getBrowserName();
-    String browserVersion = caps.getVersion();
+		System.out.println("Opening browser: " + BROWSER + " for " + OperatingSystem.toUpperCase().toString());
 
-    // Get OS name.
-    String os = System.getProperty("os.name").toLowerCase();
-    System.out.println("OS = " + os + ", Browser = " + browserName + " " + browserVersion);
+		// switch (BROWSER) {
+		// case "CH":
+		// base.driver = openChrome();
+		// break;
+		// case "FF":
+		// base.driver = openFirefox();
+		// System.out.println("Opened FF");
+		// break;
+		// default:
+		// base.driver = openFirefox();
+		// }
+	}
 
-    base.driver.quit();
-  }
+	@After
+	public void TearDownTest(Scenario scenario) {
+		if (scenario.isFailed()) {
+			// Scenario fallido
+		}
+		// Get Browser name and version.
+		Capabilities caps = ((RemoteWebDriver) base.driver).getCapabilities();
+		String browserName = caps.getBrowserName();
+		String browserVersion = caps.getVersion();
 
-  private WebDriver openChrome() {
-    // Passing the real Chrome webdriver
-    System.out.println("Opening the browser : Chrome");
-    setBrowserProperty(ChromeDriverType, ChromeDriverFile);
-    // driver se puede usar en LoginStep por dependency injection usando
-    // picocontainer
-    // (hook y loginstep extienden base)
-    return new ChromeDriver();
-  }
+		System.out
+				.println("OS = " + OperatingSystem.toUpperCase() + ", Browser = " + browserName + " " + browserVersion);
 
-  private WebDriver openFirefox() {
-    // Passing the real Firefox webdriver
-    System.out.println("Opening the browser : Firefox");
-    setBrowserProperty(FirefoxDriverType, FirefoxDriverFile);
-    System.out.println("Opened: Firefox");
-    // driver se puede usar en LoginStep por dependency injection usando
-    // picocontainer
-    // (hook y loginstep extienden base)
-    return new FirefoxDriver();
-  }
+		base.driver.quit();
+	}
 
-  private void setBrowserProperty(String driverType, String driverLocation) {
-    System.setProperty(driverType, driverLocation);
-  }
+	private WebDriver openBrowser() {
+		// Passing the real webdriver for the browser selected
+		System.out.println("Before setting up browser driver for browser: " + BROWSER);
 
+		System.setProperty(DriverType, DriverFile);
+		// driver se puede usar en LoginStep por dependency injection usando
+		// picocontainer
+		// (hook y loginstep extienden base)
+		if (BROWSER == "CH") {
+			return new ChromeDriver();
+		} else if (BROWSER == "FF") {
+			return new FirefoxDriver();
+		} else
+			return new FirefoxDriver();
+	}
+
+	// private WebDriver openChrome() {
+	// // Passing the real Chrome webdriver
+	// System.out.println("Opening the browser : Chrome");
+	// setBrowserProperty(ChromeDriverType, ChromeDriverFile);
+	// // driver se puede usar en LoginStep por dependency injection usando
+	// // picocontainer
+	// // (hook y loginstep extienden base)
+	// return new ChromeDriver();
+	// }
+	//
+	// private WebDriver openFirefox() {
+	// // Passing the real Firefox webdriver
+	// System.out.println("Opening the browser : Firefox");
+	// setBrowserProperty(FirefoxDriverType, FirefoxDriverFile);
+	// System.out.println("Opened: Firefox");
+	// // driver se puede usar en LoginStep por dependency injection usando
+	// // picocontainer
+	// // (hook y loginstep extienden base)
+	// return new FirefoxDriver();
+	// }
 }
